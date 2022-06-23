3554b023be0e58f4bb29607c0f23c952682f88e0
sohype.mop@gmail.com <sohype.mop@gmail.com>
2022-06-23 03:18:15 +0200

    refactor structure

--- deploy/accounts/urls_profile.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from accounts.views import UpdateProfileInfoView, ChangeEmailView, PhoneUpdateView
-
-urlpatterns = [
-    path('update/', UpdateProfileInfoView.as_view(), name='update_profile'),
-    path('email/update/', ChangeEmailView.as_view(), name='update_email'),
-    path('phone/update/', PhoneUpdateView.as_view(), name='update_phone'),
-]
